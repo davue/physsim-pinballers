e3ff16ffb8765bcf2858d30d1c5cf4872b1035d9
Jeremy Bitar <bitarj@student.ethz.ch>
2023-12-21 15:17:59 +0100

    split drawing stuff out of simulated object

---
+++ Drawable/DrawableObject.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Pinballers.Physics.Shapes;
+
+namespace Pinballers.Physics;
+
+public abstract class DrawableObject : DrawableGameComponent
+{
+    protected new PinballGame Game { get; }
+    public abstract Color ObjectColor { get; }
+    protected abstract Shape ObjectShape { get; }
+
+    protected DrawableObject(PinballGame game) : base(game)
+        => Game = game;
+
+    public override void Draw(GameTime gameTime)
+        => ObjectShape.Draw(Game.SpriteBatch, gameTime, ObjectColor);
+
+    protected override void Dispose(bool disposing)
+    {
+        Game.Components.Remove(this);
+        base.Dispose(disposing);
+    }
+}
---
+++ Drawable/DrawableShape.cs
@@ -0,0 +1,13 @@
+﻿using Pinballers.Physics;
+using Pinballers.Physics.Shapes;
+
+namespace Pinballers.Drawable
+{
+    public abstract class DrawableShape<T> : DrawableObject where T : Shape
+    {
+        protected sealed override Shape ObjectShape => Shape;
+        public abstract T Shape { get; }
+
+        protected DrawableShape(PinballGame game) : base(game) { }
+    }
+}
